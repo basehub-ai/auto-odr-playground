d94797a23f46c67fc4bb1c244702ae0275391c29
Julian Benegas <julianbenegas99@gmail.com>
2024-10-02 20:32:01 -0300

    add rich text

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
+import { RichText } from "basehub/react-rich-text";
 import { Pump } from "basehub/react-pump";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <Pump queries={[{ homepage: { heroTitle: true, info: true } }]}>
+      <Pump
+        queries={[
+          {
+            homepage: {
+              heroTitle: true,
+              info: true,
+              heroSubtitle: {
+                json: { content: true },
+              },
+            },
+          },
+        ]}
+      >
         {async ([{ homepage }]) => {
           "use server";
 
@@ -14,6 +27,14 @@ export default function Home() {
                 <code>{JSON.stringify({ info: homepage.info }, null, 2)}</code>
               </pre>
               <h1>{homepage.heroTitle}</h1>
+              <div style={{ maxWidth: "700px", fontSize: "13px" }}>
+                <RichText
+                  content={homepage.heroSubtitle?.json?.content}
+                  components={{
+                    p: (props) => <p {...props} style={{ margin: "8px 0" }} />,
+                  }}
+                />
+              </div>
             </>
           );
         }}
